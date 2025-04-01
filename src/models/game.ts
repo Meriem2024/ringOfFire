2396b325b06552b29c66c2c209d8870b5fe217fe
Meriem W. <meriem.ben@gmx.de>
2025-04-01 11:13:30 +0200

    Added shuffle function to Game model and improved deck initialization

--- src/models/game.ts
+++ src/models/game.ts
@@ -3,4 +3,30 @@ export class Game {
   public stack: string[] = [];
   public playedCard: string[] = [];
   public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('spade_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('diamonds_' + i);
+    }
+    shuffle(this.stack);
+  }
+}
+
+function shuffle(array: string[]) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
